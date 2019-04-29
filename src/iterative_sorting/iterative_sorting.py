3e5fa5a86956225aec2d2da2dbcbb69e043f5d54
kjohna <43828763+kjohna@users.noreply.github.com>
2019-04-29 14:22:42 -0600

    selection_sort passing tests

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,38 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+# TO-DO: Complete the selection_sort() function below
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
+        for j in range(i + 1, len(arr)):
+            tmp = arr[j]
+            if arr[j] < arr[i]:
+                arr[j] = arr[i]
+                arr[i] = tmp
+        # cur_index = i
+        # smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
+        # (hint, can do in 3 loc)
+        # for j in range(i + 1, len(arr)):
+        #     tmp = arr[j]
+        #     # if tmp < arr[smallest_index]:
+        #     if tmp < arr[i]:
+        #         # arr[j] = arr[cur_index]
+        #         # arr[cur_index] = tmp
+        #         arr[j] = arr[i]
+        #         arr[i] = tmp
+        #     j += 1
 
         # TO-DO: swap
-
-
-
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+def bubble_sort(arr):
 
     return arr
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+def count_sort(arr, maximum=-1):
 
     return arr
