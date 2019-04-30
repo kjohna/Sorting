5dcecf0798a0ccd33fcbaef78f8a7ef57ca708cf
kjohna <43828763+kjohna@users.noreply.github.com>
2019-04-30 14:19:18 -0600

    first pass rec_sort

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,16 +1,32 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
+def merge(arrA, arrB):
+    elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
     # TO-DO
-    
+    # loop for # of total elements
+    for i in range(0, elements):
+        # if arrA[0] < arrB[0], merged_arr[i] = arrA.pop(0)
+        if arrA[0] < arrB[0]:
+            merged_arr[i] = arrA.pop(0)
+            # if arrA is now empty:
+            if not len(arrA):
+                arrA.append(float('Inf'))
+        # else merged_arr[i] = arrB.pop(0)
+        else:
+            merged_arr[i] = arrB.pop(0)
+            # if arrB is now empty:
+            if not len(arrB):
+                arrB.append(float('Inf'))
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
+def merge_sort(arr):
     # TO-DO
-
+    if len(arr) > 1:
+        arrA = merge_sort(arr[:len(arr) // 2])
+        arrB = merge_sort(arr[len(arr) // 2:])
+        merge(arrA, arrB)
     return arr
 
 
@@ -20,7 +36,8 @@ def merge_in_place(arr, start, mid, end):
 
     return arr
 
-def merge_sort_in_place(arr, l, r): 
+
+def merge_sort_in_place(arr, l, r):
     # TO-DO
 
     return arr
@@ -28,6 +45,6 @@ def merge_sort_in_place(arr, l, r):
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+def timsort(arr):
 
     return arr
