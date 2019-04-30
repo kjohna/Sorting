e8e95aacf3fbadb83d312d07eed1d6ac45b88510
kjohna <43828763+kjohna@users.noreply.github.com>
2019-04-30 15:32:13 -0600

    merge_in_place passing tests

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -33,13 +33,22 @@ def merge_sort(arr):
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
     # TO-DO
-
+    # loop from start to mid
+    for i in range(start, mid + 1):
+        # loop from end to mid, swap if smaller
+        for j in range(end, mid - 1, -1):
+            if arr[j] < arr[i]:
+                arr[i], arr[j] = arr[j], arr[i]
     return arr
 
 
 def merge_sort_in_place(arr, l, r):
     # TO-DO
-
+    if r - l > 0:
+        mid = (r + l) // 2
+        arrA = merge_sort_in_place(arr, l, mid)
+        arrB = merge_sort_in_place(arr, mid + 1, r)
+        arr = merge_in_place(arr, l, mid, r)
     return arr
 
 
